0e5c8175623f57a03571b3bf1d1a909d6aa00579
fundamentalpriciple <princedixit931@gmail.com>
2025-11-08 10:55:57 +0530

    trying post request to add users

--- application/api.py
+++ application/api.py
@@ -1,8 +1,10 @@
 from flask_restful import Resource
 from flask_restful import fields, marshal_with
+from flask_restful import reqparse
 
 from application.database import db
 from application.models import User
+from application.validation import NotFoundError, BusinessValidationError
 
 output_fields = {
         "user_id": fields.Integer,
@@ -10,7 +12,11 @@ output_fields = {
         "roles": fields.String
     }
 
+create_user_parser = reqparse.RequestParser()
+create_user_parser.add_argument('username')
+
 class UserAPI(Resource):
+
     @marshal_with(output_fields)
     def get(self, username):
         user = db.session.query(User).filter(User.username == username).first()        
@@ -18,7 +24,7 @@ class UserAPI(Resource):
         if user:
             return user
         else:
-            return {}, 404
+            raise NotFoundError(status_code=404) 
 
     def put(self, username):
         pass
@@ -27,5 +33,12 @@ class UserAPI(Resource):
         pass
     
     def post(self):
-        pass
-  
+        args = create_user_parser.parse_args()
+        username = args.get("username", None)
+        
+        if username is None:
+            raise BusinessValidationError(status_code=400, error_code='BE1001', error_message="username is required")
+        
+        pass         
+
+
